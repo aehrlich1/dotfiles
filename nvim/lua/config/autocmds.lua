f34a6574a2f4402236233f02e83ebae469e79fc8
aehrlich1 <anatol.ehrlich@gmail.com>
2026-07-07 21:47:34 +0200

    Have snacks explorer open when opening directories

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Open the snacks explorer sidebar when starting nvim in a directory (or with no file)
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("auto_open_explorer", { clear = true }),
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg == "" or vim.fn.isdirectory(arg) == 1 then
+      require("snacks").explorer()
+    end
+  end,
+})
