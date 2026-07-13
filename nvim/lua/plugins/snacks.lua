56ed1325115d9638257cd49bb7ad4395ba04dff6
aehrlich1 <anatol.ehrlich@gmail.com>
2026-07-13 09:47:47 +0200

    Remap <leader><space> to Smart Find Files in Nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,14 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader><space>",
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = "Smart Find Files",
+    },
+  },
   opts = {
     indent = {
       indent = {
